84b9cf5927fa1f79465a0521dba409e5124e9c1b
aniket21mathur <aniketmathur320@gmail.com>
2018-07-16 02:09:38 +0530

    cookie+valida.

--- index.js
+++ index.js
@@ -104,3 +104,80 @@ if (!illegalChars4.test(cty))
 
 return true;
 }
+
+
+//cookies
+function SetCke (name,value,expires,path,domain,secure) 
+			{
+
+			  document.cookie = name + "=" + escape (value) +
+
+			    ((expires) ? "; expires=" + expires.toGMTString() : "") +
+
+			    ((path) ? "; path=" + path : "") +
+
+			    ((domain) ? "; domain=" + domain : "") +
+
+			    ((secure) ? "; secure" : "");
+
+			}
+
+
+			function DlteCke (name,path,domain) 
+			{
+
+			  if (GetCookie(name)) {
+
+			    document.cookie = name + "=" +
+
+			      ((path) ? "; path=" + path : "") +
+
+			      ((domain) ? "; domain=" + domain : "") +
+
+			      "; expires=Thu, 01-Jan-70 00:00:01 GMT";
+
+			  }
+
+			}
+
+
+			function GtCke (name) 
+			{
+
+			  var arg = name + "=";
+
+			  var alen = arg.length;
+
+			  var clen = document.cookie.length;
+
+			  var i = 0;
+
+			  while (i < clen) {
+
+			    var j = i + alen;
+
+			    if (document.cookie.substring(i, j) == arg)
+
+			      return getCookieVal (j);
+
+				i = document.cookie.indexOf(" ", i) + 1;
+
+			    if (i == 0) break;
+
+			  }
+
+			  return null;
+
+			}
+
+
+			function gtCkeVl (offset)
+			{
+
+				var str = document.cookie.indexOf(";", offset);
+
+				if(str==-1) str = document.cookie.length;
+				
+				return unescape(document.cookie.substring(offset, str));
+			
+			}
